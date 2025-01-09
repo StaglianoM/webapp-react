3abf797c9882e54076173ddc2d6f65de046c6964
StaglianoM <stagliano.m92@gmail.com>
2025-01-09 19:20:39 +0100

    add logo + class active

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 
 function Nav() {
     return <nav>
-        <ul>
+        <ul className='infoNav'>
             <li>
                 <NavLink to="/">Home</NavLink>
             </li>
--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,8 +1,15 @@
+import { Link } from 'react-router-dom';
 import Nav from '../components/Nav';
 
+
 function Header() {
-    return <header>
-        <Nav />
+    return <header className='navbar'>
+        <div className='containerNav'>
+            <Link to='/'>
+                <img src="/public/vite.svg" alt="" width={35} />
+            </Link>
+            <Nav />
+        </div>
     </header>
 }
 
